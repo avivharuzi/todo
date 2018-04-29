377466288192246fd3e2bd9a1aa048c188082070
avivharuzi <avivharuzi@gmail.com>
2018-04-29 15:28:44 +0300

    added todo form component

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <form @submit.prevent="onSubmit" autocomplete="off" novalidate>
+      <div class="form-group">
+        <input v-model="task" type="text" class="form-control form-control-lg" placeholder="Task">
+      </div>
+      <button class="btn btn-success btn-lg btn-block">Submit</button>
+    </form>
+  </section>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        task: null
+      }
+    },
+    methods: {
+      onSubmit () {
+        //
+      }
+    }
+  }
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="container mt-5">
-    <app-header></app-header>
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <app-header class="mb-3"></app-header>
+        <app-todo-form></app-todo-form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import Header from '@/components/Header'
+  import TodoForm from '@/components/TodoForm'
 
   export default {
     components: {
-      'app-header': Header
+      'app-header': Header,
+      'app-todo-form': TodoForm
     }
   }
 </script>
