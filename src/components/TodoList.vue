2d956c8c0bf2809a8ed9351ab77edcb34e7551ba
avivharuzi <avivharuzi@gmail.com>
2018-04-29 15:52:32 +0300

    new delete feature for todo

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,16 +1,24 @@
 <template>
   <ul v-if="tasks && tasks.length" class="list-group">
-    <li v-for="(task, i) in tasks" :key="i" class="list-group-item">{{ task }}</li>
+    <li v-for="(task, i) in tasks" :key="i" class="list-group-item">{{ task }}
+    <span class="float-right"><button @click="deleteTask(task)" class="btn btn-danger"><i class="fa fa-trash"></i></button></span>
+    </li>
   </ul>
 </template>
 
 <script>
   import { mapGetters } from 'vuex'
+  import { mapMutations } from 'vuex'
 
   export default {
     computed: {
       ...mapGetters([
         'tasks'
+      ])
+    },
+    methods: {
+      ...mapMutations([
+        'deleteTask'
       ])
     }
   }
